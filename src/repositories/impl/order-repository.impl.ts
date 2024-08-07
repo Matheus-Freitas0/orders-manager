7d358ee809c6f5f63e92fc92c95e48df67e57085
MatheusFreitas <moncaomatheus@gmail.com>
2024-08-07 17:32:43 -0300

    "Implemented create method in OrderRepositoryImpl and added getByOrder method to OrderRepository interface"

--- src/repositories/impl/order-repository.impl.ts
+++ src/repositories/impl/order-repository.impl.ts
@@ -1,15 +1,22 @@
 import { Order } from '../../models/order'
 import { OrderRepository } from '../order.repository'
 import { Repository } from '../repository'
+import queries from '../../../files/order-queries.json'
 
 export class OrderRepositoryImpl extends Repository implements OrderRepository {
 
     async create(order: Order): Promise<string> {
-        // todo -> criar a query de insert
-        // todo -> executar a query
-        // todo -> buscar order que foi criada para retornar o codigo        
-
-        return ''
+        await this.datasource.query(queries.create, order.code , order.customerId)
+        return await this.getByOrder(order.code)
     }
+
+    async getByOrder(code:string): Promise<string>{
+        const result = await this.datasource.query(queries.getByOrder, code)
+        const resultSet = result[0] 
+        return resultSet 
+
+    }
+
+
 
 }
--- src/repositories/order.repository.ts
+++ src/repositories/order.repository.ts
@@ -1,8 +1,8 @@
-import { OrderItemRequest } from '../dto/order-item-request.dto'
 import { Order } from '../models/order'
 
 export interface OrderRepository {
 
     create(order: Order): Promise<string>
-
+    getByOrder(code:string): Promise<string>
+    
 }
