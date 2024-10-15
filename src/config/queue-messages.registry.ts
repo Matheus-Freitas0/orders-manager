a926aef5837b60fcf53b32c54528549b3444c244
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-15 16:03:35 -0300

    Updated MessagePublisher interface and QueueMessagesRegistry class

--- src/config/queue-messages.registry.ts
+++ src/config/queue-messages.registry.ts
@@ -1,6 +1,5 @@
 import orderPaymentQueueRegister from '../queue/order-payment-queue'
 import { Inject } from './container.config'
-import { MessagePublisher } from './messaging/message-publisher'
 import { RabbitMQAdapter } from './messaging/rabbitmq.adapter'
 
 export class QueueMessagesRegistry {
@@ -11,7 +10,7 @@ export class QueueMessagesRegistry {
     constructor () {}
     
     async register(): Promise<void> {
-        const rabbitAdapter = this.rabbitMQAdapter as RabbitMQAdapter
+        const rabbitAdapter = this.rabbitMQAdapter
 
         await orderPaymentQueueRegister(rabbitAdapter)
         
