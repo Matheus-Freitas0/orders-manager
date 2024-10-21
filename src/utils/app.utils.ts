a5301fa7fe29de3a36ae7052edaf0a700e2fa34d
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-10-21 16:08:12 -0300

    Added multiply and average methods to AppUtils class

--- src/utils/app.utils.ts
+++ src/utils/app.utils.ts
@@ -28,4 +28,30 @@ export class AppUtils {
       return prev + current;
     }, 0);
   }
+
+  static multiply(...numbers: any[]): number {
+    const hasNegative = numbers.some((item) => item < 0);
+    if (hasNegative) throw new Error("nao pode negativo");
+
+    const justNumbers = numbers.every((item) => typeof item === "number");
+    if (!justNumbers) throw new Error("so pode numeros");
+
+    return numbers.reduce((prev, current) => {
+      return prev * current;
+    }, 1);
+  }
+  
+  static average(...numbers: any[]): number {
+    if (numbers.length === 0) throw new Error("lista nao pode estar vazia");
+
+    const justNumbers = numbers.every((item) => typeof item === "number");
+    if (!justNumbers) throw new Error("so pode numeros");
+
+    const total = numbers.reduce((prev, current) => {
+      return prev + current;
+    }, 0);
+
+    return total / numbers.length;
+  }
+
 }
