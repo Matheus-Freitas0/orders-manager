0ab6b0298a3fecd5138a65ee70c3b69bf4ebf115
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-23 14:22:00 -0300

    Removed OrderValidatorPayImpl class and updated OrderValidatorStrategy to inject CustomerService and retrieve customer data in execute method.

--- src/validators/order-validator.strategy.ts
+++ src/validators/order-validator.strategy.ts
@@ -1,6 +1,7 @@
 import { Inject, InjectArray } from '../config/container.config'
 import { OrderErrorDTO } from '../dto/order-error.dto'
 import { OrderRequest } from '../dto/order-request.dto'
+import { CustomerService } from '../services/customer.service'
 import { ProductService } from '../services/product.service'
 import { OrderValidator } from './order.validator'
 
@@ -8,11 +9,13 @@ export class OrderValidatorStrategy {
 
     @InjectArray('orderValidator') private orderValidators!: OrderValidator[]
     @Inject('productSvc') private productService!: ProductService
-    
+    @Inject('customerSvc') private customerService!: CustomerService
+
     async execute(orderRequest: OrderRequest, orderMetadata?: { [name: string]: any }): Promise<OrderErrorDTO[]> {
         let outerErrors: OrderErrorDTO[] = []
         
         orderMetadata!['products'] = await this.productService.getProductsByCodeIn(orderRequest.items.map(item => item.productCode))
+        orderMetadata!['customer'] = await this.customerService.getByDocument(orderRequest.customerDocument)
         
         for (const validator of this.orderValidators) {
             const innerErrors = await validator.validate(orderRequest, orderMetadata)
